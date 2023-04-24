1ac216d76169f0453d194485d1c404ca3630c7c9
siva <mangalisivapullaiah@gmail.com>
2023-04-24 11:10:03 +0530

    day 6 class work

---
+++ Day-6/index.py
@@ -0,0 +1,41 @@
+def add():
+    try:
+        a=int(input())
+    except ValueError as err:
+        print(err)
+    try:
+        b=int(input())
+    except ValueError as err:
+        print(err)
+    try:
+        print(a+b)
+    except Exception as e:
+        print(e,"siva")
+
+# add()
+
+def test(*d):
+    #print(c)
+    for each in d:
+        print(each)
+# test(1,2)
+
+def fib(n):
+    first=0
+    second=1
+    
+    for each in range(n):
+        if each==0:
+            print(0)
+        elif each==1:
+            print(1)
+        else:
+            temp=first+second
+            print(temp)
+            first=second
+            second=temp
+
+try:
+    fib(int(input()))
+except Exception as e:
+    print(e)
