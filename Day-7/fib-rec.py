7b53e3df96065178499663f3c1607d527282139d
siva <mangalisivapullaiah@gmail.com>
2023-04-25 10:53:06 +0530

    fibonacci sequence and sum return

--- Day-7/fib-rec.py
+++ Day-7/fib-rec.py
@@ -1,3 +1,4 @@
+# problem 1
 def nth_fib_rec(n):
     if n==0:
         return -1
@@ -6,7 +7,39 @@ def nth_fib_rec(n):
     if n==2:
         return 1
     else:
-        return fib_rec(n-1)+fib_rec(n-2)
+        return nth_fib_rec(n-1)+nth_fib_rec(n-2)
+
+# n = int(input())
+# print(nth_fib_rec(n))
+
+# problemm 2
+
+def fib_seq(n):
+    if n<=1:
+        return n
+    else:
+        return fib_seq(n-1)+fib_seq(n-2)
+
+# n = int(input())
+# if n<=0:
+#     print("Enter valid statement")
+# else:
+#     for fib in range(n):
+#         print(fib_seq(fib), end=' ')
+
+# problem 3
+def sum_fib(n):
+    if n<=1:
+        return n
+    else:
+        return sum_fib(n-1)+sum_fib(n-2)
 
 n = int(input())
-print(nth_fib_rec(n))
+sum=0
+if n<=0:
+    print("Enter valid input")
+else:
+    for num in range(n):
+        sum+=sum_fib(num)
+
+print(sum)
